4800e9b5b4e55b523b6e3fc05dd3310f8a3a5b74
Matt Hunzinger <matthunz2@gmail.com>
2023-06-12 18:41:11 -0400

    Add Task::block_on method

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -16,4 +16,33 @@ pub trait Task {
     type Output;
 
     fn poll(self: Pin<&mut Self>) -> Poll<Self::Output>;
+
+    fn poll_unpin(mut self) -> Poll<Self::Output>
+    where
+        Self: Sized + Unpin,
+    {
+        Pin::new(&mut self).poll()
+    }
+
+    fn block_on(mut self) -> Self::Output
+    where
+        Self: Sized + Unpin,
+    {
+        loop {
+            if let Poll::Ready(output) = (&mut self).poll_unpin() {
+                break output;
+            }
+        }
+    }
+}
+
+impl<T> Task for &mut T
+where
+    T: ?Sized + Task + Unpin,
+{
+    type Output = T::Output;
+
+    fn poll(mut self: Pin<&mut Self>) -> Poll<Self::Output> {
+        Pin::new(&mut **self).poll()
+    }
 }
